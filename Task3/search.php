0349f2d274dbac43f31460d9f52cf39b99059f4f
Daud Sayyed <sayyeddaud@gmail.com>
2014-07-17 18:56:42 +0530

    Last Task Completed

---
+++ Task3/search.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+ <meta charset="utf-8">
+
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+<link rel="icon" href="google.ico">
+
+<style>
+#subj{text-decoration:underline;color:#2a388f;font-weight: bold;}
+.row .container a:hover{text-decoration: none;color:#000;}
+#ads pre{clear:both;background:none;border:none;margin:0px;padding:0px;}
+.form-control:focus{box-shadow: 1px 0px 0px #2980b9;}
+ul{margin-top: -40px; margin-left:-63px; }
+ul li{text-decoration: none; display: inline-block;color:#4c4c4c;float: left; padding: 15px; font-size: 0.7em;}
+ul li a{color:#4c4c4c;}
+ul li:active{}
+</style>
+</head>
+<title>moto g - Google Search</title>
+<body>
+<div class="container" style="width:100%;border:0px solid red;">
+
+<div class="row" style="background:#f1f1f2;padding:15px;">
+<! Google logo>
+<div id="logo" class="col-md-1" >
+	<a href="https://www.google.co.in/webhp?tab=ww&ei=aVzHU5CaFdCdugTDxoHIBQ&ved=0CBQQ1S4" title="Home">
+	<img src="GOOGLE.png" width=100/>
+	</a>
+</div>
+<! Search Bar>
+<div class="col-md-9">
+		<form >
+        <div class="col-md-8 input-group">
+        <input style="border-radius: 1px;" type="text" class="form-control" placeholder="" value="moto g" name="search">
+        <div  class="input-group-btn">
+        <button style="width:60px; background:#3498db; color:#fff; border-radius: 2px;" class="btn" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+        </div>
+        </div>
+        </form>
+        </div>
+
+<div id="side" class="col-md-2" style="border:0px solid red;">
+
+ <a style="display:inline-block;color:#000;" href="">+Daud</a>
+<a  style="display:inline-block;margin-left:5px;" href="" ><img src="app.png"/></a>
+<a  style="display:inline-block;margin-left:5px;" href="" ><img src="bell.png"/></a>
+<a  style="display:inline-block;margin-left:5px;background:#eaeaea;color:#4c4c4c" href="" ><span>Share</span></a>
+<a style="display:inline-block;" href="" ><img class="img-circle" src="photo.png"</a>
+</div>
+
+
+ 
+</div>
+
+<div class="row " style="border:0px solid red;font-size:1.5em;margin-top:-20px;">
+	<div class="container">
+
+	<ul>
+		<li style="border-bottom: 3px solid #dd4b39;"><a style="color:#dd4b39;" href=""><b>Web</b></a></li>
+		<li><a href="">News</a></li>
+		<li><a href="">Images</a></li>
+		<li><a href="">Videos</a></li>
+		<li><a href="">Maps</a></li>
+		<li><a href="">More</a></li>
+		<li><a href="">Search tools</a></li>
+	</ul>
+
+<!-- 	<div style="border-bottom:3px solid #dd4b39;;display:inline-block;color:#dd4b39;margin-left:25px;"><a href="#" style="color:#dd4b39;"><strong style="color:#dd4b39;">Web</strong></a></div>
+	<div style="display:inline-block;margin-left:15px;"><a href="#">News</a></div>
+	<div style="display:inline-block;margin-left:15px;"><a href="#">Images</a></div>
+	<div style="display:inline-block;margin-left:15px;"><a href="#">Videos</a></div>
+	<div style="display:inline-block;margin-left:15px;"><a href="#">Maps</a></div>
+	<div style="display:inline-block;margin-left:15px;"><a href="#">More<span class="caret"></span></a></div>
+	<div style="display:inline-block;margin-left:15px;"><a href="#">Search tools</a></div> -->
+</div>
+	</div>
+	<button type="button" style="width:70px; height: 30px; background: #ecf0f1; border-radius: 2px; color:#404041; float:right; margin-right: 40px; margin-top: -50px" class="btn btn-default"><span class="glyphicon glyphicon-cog"></span></button>
+	<hr style="margin-top: 0px;width=100%;"/>
+	<div class="row">
+	<p class="col-md-1"></p>
+		<small style="margin-right:15px;" class="disabled">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;About 1,18,00,00,000 results (0.33 seconds)</small>
+	</div>
+	<div class="row" style="">
+		<div class="container">
+			<div id="ads" class="col-md-6">
+			<div style="border:0px solid green;margin-left:-32px;background:#fcfcbe;padding-left:10px;moder-top:5px";>
+					ads related to <b>moto g</b><br/>
+					Buy Moto G Online - An Exceptional Smart Phone‎<br/>
+					<a href="#" style="color:blue;">Adwww.flipkart.com/Moto_G‎</a><span class="caret"></span><br/>
+					Buy Online Exclusively @ Flipkart.<br/>
+					Flipkart has 4,56,206 followers on Google+			
+			</div>
+<br/>
+
+					<div style="margin-left:-32px;">
+						<a href="#"><span id="subj">Motorola Moto G - Full phone specifications - GSM Arena</span></a>
+						<span style="color:green;">www.gsmarena.com/motorola_moto_g-5831.php</span><br>
+						Motorola Moto G Android smartphone. Announced 2013, November. Features 3G, 4.5″ IPS LCD capacitive touchscreen, 5 MP camera, Wi-Fi, GPS, Bluetooth.
+					</div>
+					<br/>
+					<div style="margin-left:-32px;">
+						<a href="#"><span id="subj">Motorola Moto G - Full phone specifications - GSM Arena</span></a>
+						<span style="color:green;">www.gsmarena.com/motorola_moto_g-5831.php</span><br>
+						Motorola Moto G Android smartphone. Announced 2013, November. Features 3G, 4.5″ IPS LCD capacitive touchscreen, 5 MP camera, Wi-Fi, GPS, Bluetooth.
+					</div>
+					<br/>
+					<div style="margin-left:-32px;">
+						<a href="#"><span id="subj">Motorola Moto G - Full phone specifications - GSM Arena</span></a>
+						<span style="color:green;">www.gsmarena.com/motorola_moto_g-5831.php</span><br>
+						Motorola Moto G Android smartphone. Announced 2013, November. Features 3G, 4.5″ IPS LCD capacitive touchscreen, 5 MP camera, Wi-Fi, GPS, Bluetooth.
+					</div>
+					<br/>
+					<div style="margin-left:-32px;">
+						<a href="#"><span id="subj">Motorola Moto G - Full phone specifications - GSM Arena</span></a>
+						<span style="color:green;">www.gsmarena.com/motorola_moto_g-5831.php</span><br>
+						Motorola Moto G Android smartphone. Announced 2013, November. Features 3G, 4.5″ IPS LCD capacitive touchscreen, 5 MP camera, Wi-Fi, GPS, Bluetooth.
+					</div>
+					<div style="margin-left:-32px;">
+						<a href="#"><span id="subj">Motorola Moto G - Full phone specifications - GSM Arena</span></a>
+						<span style="color:green;">www.gsmarena.com/motorola_moto_g-5831.php</span><br>
+						Motorola Moto G Android smartphone. Announced 2013, November. Features 3G, 4.5″ IPS LCD capacitive touchscreen, 5 MP camera, Wi-Fi, GPS, Bluetooth.
+					</div>
+					<br/>
+			
+
+							
+			</div>
+
+			<div class="col-md-4" style="float:right;">
+			<div id="sidebox" style="border:1px solid #bdc3c7;padding:15px;float:right;margin-top-200px;">
+				<p>Shop for <b>moto g</b> on Google</p><br/>
+				<div style="display:inline-block;"><img src="download.jpg"/><br/>
+					<b>moto g</b><br/><b>Rs 12,499.00</b><br/><a>Flipkart</a>
+				</div>
+
+				<div style="display:inline-block;margin-left:15px;"><img src="img1.jpg"/><br/>
+					<b>moto g</b><br/><b>Rs 12,499.00</b><br/><a>Flipkart</a>
+				</div>
+
+<div style="display:inline-block;"><img src="img2.jpg"/><br/>
+					<b>moto g</b><br/><b>Rs 12,499.00</b><br/><a>Flipkart</a>
+				</div>
+			<div style="display:inline-block;"><img src="img2.jpg"/><br/>
+					<b>moto g</b><br/><b>Rs 12,499.00</b><br/><a>Flipkart</a>
+				</div>
+				<div style="display:inline-block;"><img src="img2.jpg"/><br/>
+					<b>moto g</b><br/><b>Rs 12,499.00</b><br/><a>Flipkart</a>
+				</div>	
+			</div>
+		</div>
+		</div>
+		
+</div>
+
+<div class="row" style="background:#f1f1f2;">
+<ul style="text-decoration: none; color:#4c4c4c; float: left;font-size: 1.5em;margin-top:10px;margin-left:10px;">
+		
+		<li><a href="">Help</a></li>
+		<li><a href="">Send feedback</a></li>
+		<li><a href="">Privacy & Terms</a></li>
+		
+	</ul>
+ </div>
+
+</body>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</html>
